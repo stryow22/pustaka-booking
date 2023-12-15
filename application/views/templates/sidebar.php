e7f569377903b9819ed7b46b35d409c4bc569edb
SATRIYO WITJAKSONO <satriyowitjaksono2205@gmail.com>
2023-12-15 19:04:32 +0700

    logout header

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,9 +2,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('home'); ?>>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('home'); ?>">
                 <div class=" sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-book"></i>
+                    <i class="fas fa-book"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Pustaka Booking</div>
             </a>
